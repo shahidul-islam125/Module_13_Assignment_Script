f01fa7242db238c0fa53a89d2a43eadd7ef3fd93
SHAHIDUL <sishahid483@gmail.com>
2023-11-06 21:23:51 +0600

    Some code has been changed

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -6,17 +6,17 @@ const MenuItem = () => {
         <div>
            <nav>
                 <ul>
-                    <li><NavLink className={(nav) => {nav.isActive ? "active-item" : "pending-item"}} to='/'>Home</NavLink></li>
+                    <li><NavLink className={({isActive}) => (isActive ? "active-item" : "pending-item")} to='/'>Home</NavLink></li>
 
-                    <li><NavLink className={(nav) => {nav.isActive ? "active-item" : "pending-item"}} to='/about'>About</NavLink></li>
+                    <li><NavLink className={({isActive}) => (isActive ? "active-item" : "pending-item")} to='/about'>About</NavLink></li>
 
-                    <li><NavLink className={(nav) => {nav.isActive ? "active-item" : "pending-item"}} to='/blog'>Blog</NavLink></li>
+                    <li><NavLink className={({isActive}) => (isActive ? "active-item" : "pending-item")} to='/blog'>Blog</NavLink></li>
 
-                    <li><NavLink className={(nav) => {nav.isActive ? "active-item" : "pending-item"}} to='/contact'>Contact</NavLink></li>
+                    <li><NavLink className={({isActive}) => (isActive ? "active-item" : "pending-item")} to='/contact'>Contact</NavLink></li>
 
-                    <li><NavLink className={(nav) => {nav.isActive ? "active-item" : "pending-item"}} to='/project'>Project</NavLink></li>
+                    <li><NavLink className={({isActive}) => (isActive ? "active-item" : "pending-item")} to='/project'>Project</NavLink></li>
 
-                    <li><NavLink className={(nav) => {nav.isActive ? "active-item" : "pending-item"}} to='/service'>Service</NavLink></li>
+                    <li><NavLink className={({isActive}) => (isActive ? "active-item" : "pending-item")} to='/service'>Service</NavLink></li>
                 </ul>
            </nav>
         </div>
